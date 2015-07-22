881507d0c476185db39d94de0cd1751dc9579d6d
Max Irwin <max_irwin@yahoo.com>
2015-07-22 15:46:26 +0100

    static1437576386

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-find . -name \*~ | xargs rm -rf
 wintersmith build
 mv build compiled
 git add compiled
 suffix=$(date +%s)
 commitname=static$suffix
-git commit -m '$commitname'
+git commit -m $commitname
+git push origin master
 ssh ubuntu@max.io "./gitblog.sh"
