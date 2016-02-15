5b5a97fcb0802cc9911079e5b51c9984a2257562
Max Irwin <max_irwin@yahoo.com>
2016-02-15 18:23:45 +0000

    Twitter metadata

---
+++ tools.js
@@ -0,0 +1,11 @@
+var config = JSON.parse(require('fs').readFileSync('config.json'));
+
+var tools = module.exports = {};
+
+tools.image = function(str) {
+	return img || "http://binarymax.com/star200.png";
+};
+
+tools.description = function(str) {
+	return str || config.locals.description;
+};
