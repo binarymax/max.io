8ed561bd93c2f2870623ab02f30995b956df2167
Max Irwin <max@max.io>
2025-06-15 19:15:25 -0400

    static1750029325

--- compile.sh
+++ compile.sh
@@ -6,4 +6,4 @@ suffix=$(date +%s)
 commitname=static$suffix
 git commit -m $commitname
 git push origin master
-ssh -i ~/aws/RSVP.pem ubuntu@maxirwin.com "./gitblog.sh"
+ssh -i ~/aws/RSVP.pem ubuntu@maxirwin.com "sudo ./gitblog.sh"
